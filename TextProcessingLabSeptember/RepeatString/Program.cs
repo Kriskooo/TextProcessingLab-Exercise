aafa7df761843854cd4d986d70e55f3e9f55bbe6
Kriskooo <kris.danielov@gmail.com>
2020-11-20 16:44:38 +0200

    TextProcessingLab-Exercise

---
+++ TextProcessingLabSeptember/RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string result = string.Empty;
+
+            foreach (var word in input)
+            {
+                int lenght = word.Length;
+                for (int i = 0; i < lenght; i++)
+                {
+                    result += word;
+                }
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TextProcessingLabSeptember/TextProcessingLabSeptember/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TextProcessingLabSeptember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string current;
+            while ((current = Console.ReadLine()) != "end")
+            {
+                string reversed = "";
+                for (int i = current.Length - 1; i >= 0; i--)
+                {
+                    reversed += current[i];
+                }
+                string result = string.Join(" = ", current, reversed.ToString());
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
